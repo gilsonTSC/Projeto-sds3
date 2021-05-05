6ff0724c102d57c5d34fbcfb31459a9d8796695e
Gilson Trajano <gilson.costa@dce.ufpb.br>
2021-05-05 19:15:49 -0300

    Layers

---
+++ backend/src/main/java/com/gilsontsc/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.gilsontsc.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gilsontsc.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
