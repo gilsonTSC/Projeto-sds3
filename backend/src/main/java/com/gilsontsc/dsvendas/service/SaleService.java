71d10a8b99243ba6f8799c9a5676e1ab6439a76b
Gilson Trajano <gilson.costa@dce.ufpb.br>
2021-05-06 18:43:34 -0300

    Group by search

--- backend/src/main/java/com/gilsontsc/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/gilsontsc/dsvendas/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.gilsontsc.dsvendas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.gilsontsc.dsvendas.dto.SaleDTO;
+import com.gilsontsc.dsvendas.dto.SaleSuccessDTO;
+import com.gilsontsc.dsvendas.dto.SaleSumDTO;
 import com.gilsontsc.dsvendas.entities.Sale;
 import com.gilsontsc.dsvendas.repositories.SaleRepository;
 import com.gilsontsc.dsvendas.repositories.SellerRepository;
@@ -26,5 +30,15 @@ public class SaleService {
 		Page<Sale> result = repository.findAll(pageable);
 		
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
 	}
 }
